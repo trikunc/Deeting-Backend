ecc13bfb395ba5513c2e77d816a24d282a50d22a
hananloser <hasyrawi@gmail.com>
2021-08-02 12:23:26 +0800

    adding minimal test jargon with jest

---
+++ src/__tests__/database/database.test.ts
@@ -0,0 +1,8 @@
+let testing: boolean = true
+
+describe("Testing knex database functional", () => {
+    test("It should be true", () => {
+        const actual = true
+        expect(actual).toEqual(true)
+    });
+});
