315bda8230e15432a8e62923715edaed0a9df82d
hananloser <hasyrawi@gmail.com>
2021-08-08 20:15:26 +0800

    adding service to upload s3

---
+++ src/services/aws/upload.s3.ts
@@ -0,0 +1,41 @@
+import aws from "aws-sdk"
+import multer from "multer"
+import multers3 from "multer-s3"
+
+/**
+ * AWS CONFIG 
+*/
+let config = {
+    secretAccessKey: process.env.AWS_ACCESS_KEY_ID as string,
+    accessKeyId: process.env.AWS_SECRET_KEY as string,
+    region: process.env.AWS_REGION_NAME as string
+}
+
+const s3 = new aws.S3()
+
+const fileFilter = (req: any, file: any, cb: Function) => {
+    if (file.mimeType == "image/jpeg" || file.mimeType == "image/png") {
+        cb(null, true)
+    } else {
+        cb(new Error("Invalid File Type"), false)
+    }
+}
+
+const upload = multer({
+    fileFilter,
+    storage: multers3({
+      acl: 'public-read',
+      s3,
+      bucket: 'bwm-ng-dev',
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: 'TESTING_METADATA'});
+      },
+      key: function (req, file, cb) {
+        cb(null, Date.now().toString())
+      }
+    })
+  });
+  
+  module.exports = upload;
+
+
