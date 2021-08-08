5c40044ae9e998de8a5bd6085a466a0d63f8570b
hananloser <hasyrawi@gmail.com>
2021-08-09 02:04:03 +0800

    DEETING-56 boiler plate roles

---
+++ src/controller/roles/roles.controller.ts
@@ -0,0 +1,22 @@
+class RolesController {
+
+    // Get Roles
+    async getRoles(req: Request, res: Response) {
+        
+    }
+    // Create Roles
+    async createRoles(req: Request, res: Response) {
+
+    }
+    // Update Roles
+    async updateRoles(req: Request, res: Response) {
+
+    }
+    // Delete Roles From User / Delete Roles
+    async revokeRoles(req: Request, res: Response) {
+
+    }
+}
+
+
+export default new RolesController()
