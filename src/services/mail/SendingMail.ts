f000ad8c47f54ced9e0bda5c86692f5fdfd93295
hananloser <hasyrawi@gmail.com>
2021-08-09 23:01:02 +0800

    DEETING-57 Adding Class SendMail

---
+++ src/services/mail/SendingMail.ts
@@ -0,0 +1,13 @@
+
+
+class SendingMailService {
+
+    private to: string
+    private from: string
+
+    constructor(to: string, from: string) {
+        this.to = to
+        this.from = from
+    }
+
+}
