dd92461b237d7f227fa66b15710be06f95d3a168
hananloser <hasyrawi@gmail.com>
2021-08-10 10:51:46 +0800

    DEETING-57 create mail class and add packages nodemail for sending mail

--- src/controller/users/user.controller.ts
+++ src/controller/users/user.controller.ts
@@ -1,98 +1,31 @@
 import { User } from "entity/User"
 import { Request, Response } from "express"
+import SendingMailService from "../../services/mail/SendingMail"
 import { hashingPassword } from "../../helper/hashing_password"
 import { WebResponse } from "../../models/WebResponse"
 import { getAllUser, getUser as getUserServices, registerUser, updateProfileService } from "../../services/users/user.services"
 
 class UserController {
-    /**
-     * @swagger
-     * /users/{id}:
-     *  get:
-     *    tags:
-     *      - "user_service"
-     *    summary: "Get User by ID"
-     *    description: ""
-     *    consumes:
-     *      - "application/json"
-     *    produces:
-     *      - "application/json"
-     *    parameters:
-     *     - name: "id"
-     *       in: "path"
-     *       type : "integer"
-     *       description: "User ID"
-     *       required: true
-     *    responses:
-     *      '200':
-     *        description: "A successful response"
-     *      '500':
-     *        description: "Internal server error"
-     */
-    async getUser(req: Request, res: Response) {
+   async getUser(req: Request, res: Response) {
         let { id } = req.params
         let user = await getUserServices(id)
         return WebResponse.success(res, user)
     }
+
     /**
-     * @swagger
-     * /users:
-     *  get:
-     *    tags:
-     *      - "user_service"
-     *    summary: "Get All User"
-     *    description: ""
-     *    consumes:
-     *      - "application/json"
-     *    produces:
-     *      - "application/json"
-     *    responses:
-     *      '200':
-     *        description: "A successful response"
-     *      '500':
-     *        description: "Internal server error"
-     */
-    async users(req: Request, res: Response) {
+     * get all user
+    */
+   async users(req: Request, res: Response) {
         let users = await getAllUser()
         return WebResponse.success(res, users)
     }
-    /**
-    * @swagger
-    * /users:
-    *  post:
-    *      description: User registration
-    *      tags: [user_service]
-    *      consumes: 
-    *         - application/json
-    *      parameters:
-    *          - in: body
-    *            name: "Request Body"
-    *            description: "User Creation"
-    *            schema:
-    *              type: object
-    *              required:
-    *                  - username
-    *              properties:
-    *                  username:
-    *                      type: string
-    *                  password:
-    *                       type: string
-    *                  email:
-    *                      type: string
-    *                  avatar:
-    *                      type: string
-    *      responses:
-    *          '201':
-    *              description: user received and uploaded successfully
-    *          '400':
-    *              description: user data is missing or invalid
-    *          '500':
-    *              description: Internal server error
-    *      
-    *  
-    *           
-    */
-    async createUser(req: Request, res: Response) {
+
+   /**
+    * Create User
+    * @param {Request} req 
+    * @param {Response} res
+   */
+   async createUser(req: Request, res: Response) {
         let { body } = req
         let password = await hashingPassword(req.body.password)
         let encrypted: User = {
@@ -105,6 +38,11 @@ class UserController {
 
         try {
             await registerUser(encrypted)
+            await SendingMailService.sendMail({
+                from: "hasryawi@gmail.com",
+                to: "hasyrawi@gmail.com", subject: "Terima kasih telah mendaftar", html: "Terima kasih telah mendaftar di kami, silahkan login untuk mengakses aplikasi",
+                text: "Test"
+            })
         } catch (error) {
             return res.status(500).json({
                 message: error.message
@@ -122,7 +60,7 @@ class UserController {
      * @param {Response} res}
     */
     async updateProfile(req: Request, res: Response) {
-        
+
         let { id } = req.params
         let { body } = req
         let password = await hashingPassword(body.password)
--- src/services/mail/SendingMail.ts
+++ src/services/mail/SendingMail.ts
@@ -1,13 +1,37 @@
+import nodemailer from "nodemailer"
 
+type MailOptions = {
+    from: string,
+    to: string,
+    subject: string,
+    text: string,
+    html: string,
+}
 
 class SendingMailService {
 
-    private to: string
-    private from: string
+    private transport = nodemailer.createTransport({
+        host: "smtp.mailgun.org",
+        port: 587,
+        auth: {
+            user: "postmaster@sandboxe9f1c5368c5a4cab93950dbf8b5600e9.mailgun.org",
+            pass: "fbd64dee1d467579db62c0b775a4e7ed-c485922e-b740fb7d",
+        }
+    })
 
-    constructor(to: string, from: string) {
-        this.to = to
-        this.from = from
+    /**
+     * Sending Mail
+     * @param  {MailOptions} options
+     */
+    async sendMail(options: MailOptions) {
+        this.transport.sendMail(options, (err, info) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(info.response)
+            }
+        })
     }
-
 }
+
+export default new SendingMailService()
