8dbdfa8f91e2d09ac6b9ae2f9b7c0af0b887cb53
hananloser <hasyrawi@gmail.com>
2021-08-11 12:17:30 +0800

    DEETING-67 Adding Middleware for Assign And Revoking roles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import authController from "../controller/auth/auth.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { authValidation } from "../controller/auth/auth.validation";
 import rolesController from "../controller/roles/roles.controller";
+import { roleMiddleware } from "../middleware/role.middleware";
 
 
 const router = Router()
@@ -26,9 +27,9 @@ router.get("/users/:id", [authMiddleware], userController.getUser)
 router.post("/login", [authValidation], authController.login)
 
 // Roles
-router.post("/roles/assignRole",        rolesController.assignRoles)
-router.post("/roles/revokeRole",        rolesController.revokeRoles)
-router.get("/roles/:user_id",           rolesController.getRoles)
+router.post("/roles/assignRole", [roleMiddleware],rolesController.assignRoles)
+router.post("/roles/revokeRole", [roleMiddleware],rolesController.revokeRoles)
+router.get("/roles/:user_id",    [roleMiddleware],rolesController.getRoles)
 
 
 export default router;
